e73b7bbd084070eda2c628f28d4bfca4773d3d87
hiiiyilingzhang <jocelynzhang1119@icloud.com>
2021-10-27 15:05:34 +0800

    py.script-01 Update

---
+++ src/script/py.script/01-InitializeData.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import scanpy as sc
+
+sc.settings.verbosity = 3    # errors (0), warnings (1), info (2), hints (3)
+sc.settings.set_figure_params(dpi=80)
+
+
+
